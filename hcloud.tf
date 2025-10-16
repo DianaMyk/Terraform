1db20677387d443be5cb5b5d103ca66d4c7eb2b6
Diana Mykytyn <mykytyn.di@gmail.com>
2025-10-16 15:24:11 +0300

    added certbot with cf api

--- hcloud.tf
+++ hcloud.tf
@@ -13,8 +13,11 @@ resource "hcloud_server" "server" {
     compose = base64encode(file(var.docker_compose_file))
     vector  = base64encode(file(var.vector_config_file))
     nginx_homer   = base64encode(file(var.nginx_homer_conf))
+    nginx_logs   = base64encode(file(var.nginx_logs_conf))
     ssh_pub_key = tls_private_key.pk-rsa.public_key_openssh
+    CF_API_TOKEN = var.cloudflare_api_token
     domain_homer  = var.domain_homer
+    domain_logs  = var.domain_logs
     ssl_email     = var.ssl_email
   })
 }
